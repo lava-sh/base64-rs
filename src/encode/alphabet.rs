4641558ea65f13d2efa3990c28ab7d94729eabf6
chiri <chirizxc@proton.me>
2026-08-20 20:16:26 +0300

    update alphabet

--- src/encode/alphabet.rs
+++ src/encode/alphabet.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Copy)]
 #[repr(transparent)]
 pub struct AlphabetTable(pub [u8; 64]);
 
@@ -18,6 +19,24 @@ impl Alphabet {
         match self {
             Self::Standard => &ALPHABET,
             Self::UrlSafe => &URLSAFE_ALPHABET,
+        }
+    }
+
+    pub const fn table(self) -> AlphabetTable {
+        *self.as_bytes()
+    }
+
+    pub const fn from_altchars(altchars: [u8; 2]) -> Option<Self> {
+        match altchars {
+            [b'-', b'_'] => Some(Self::UrlSafe),
+            _ => None,
+        }
+    }
+
+    pub const fn pairs(self) -> *const u16 {
+        match self {
+            Self::Standard => super::scalar::STANDARD_PAIRS.as_ptr(),
+            Self::UrlSafe => super::scalar::URL_SAFE_PAIRS.as_ptr(),
         }
     }
 }
