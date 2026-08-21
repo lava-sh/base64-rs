25a35e6e131ff904fd856c503153a230a1b3fe01
chiri <chirizxc@proton.me>
2026-08-21 17:28:12 +0300

    try fix WASM

--- py-src/base64_rs/__init__.py
+++ py-src/base64_rs/__init__.py
@@ -1,9 +1,8 @@
+# ruff: noqa: RUF067
+
 __all__ = (
     "__version__",
-    "_b64encode_avx2",
-    "_b64encode_avx512",
     "_b64encode_scalar",
-    "_b64encode_ssse3",
     "b64encode",
     "standard_b64encode",
     "urlsafe_b64encode",
@@ -12,10 +11,22 @@ __all__ = (
 from ._base64_rs import (
     __version__,
     _b64encode as b64encode,
-    _b64encode_avx2,
-    _b64encode_avx512,
     _b64encode_scalar,
-    _b64encode_ssse3,
     _standard_b64encode as standard_b64encode,
     _urlsafe_b64encode as urlsafe_b64encode,
 )
+
+try:
+    from ._base64_rs import (
+        _b64encode_avx2,
+        _b64encode_avx512,
+        _b64encode_ssse3,
+    )
+except ImportError:
+    pass
+else:
+    __all__ += (
+        "_b64encode_avx2",
+        "_b64encode_avx512",
+        "_b64encode_ssse3",
+    )
